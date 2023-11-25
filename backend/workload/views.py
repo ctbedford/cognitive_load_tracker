543c53b67b350d5279d32974671d1c1739fd0b5e
ctbedford <tybed7@icloud.com>
2023-11-25 00:39:42 -0600

    Added lots of support including CI CD

--- backend/workload/views.py
+++ backend/workload/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Task
 
 # Create your views here.
+def task_list(request):
+    tasks = Task.objects.all().values('id', 'title', 'description', 'completed')
+    tasks_list = list(tasks)
+    return JsonResponse(tasks_list, safe=False)
